253e483878981e5dd5b7e95dcf6c852ff0bb4a11
yasin <yasinazal@outlook.de>
2025-04-19 15:08:17 +0200

    Initial

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'ibsys2' title`, () => {
+  it(`should have the 'Ibsys2' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ibsys2');
+    expect(app.title).toEqual('Ibsys2');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ibsys2');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Ibsys2');
   });
 });
